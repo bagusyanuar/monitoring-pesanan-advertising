35022b3e88487bf91cca388048c43115629e9306
bagusyanuar <bagus.yanuar613@gmail.com>
2024-05-28 12:26:51 +0700

    init commit

---
+++ database/migrations/2024_05_28_045705_create_penjualan.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('penjualans', function (Blueprint $table) {
+            $table->id();
+            $table->bigInteger('user_id')->unsigned()->nullable();
+            $table->string('no_penjualan')->unique();
+            $table->date('tanggal');
+            $table->bigInteger('total')->default(0);
+            $table->smallInteger('status')->default(0);
+            $table->timestamps();
+            $table->foreign('user_id')->references('id')->on('users');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('penjualans');
+    }
+};
